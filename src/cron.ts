6cd7060e5f9ba6004dca4e51582767974295be1e
Juan Di Toro <ditorojuanf@gmail.com>
2019-06-14 22:14:32 -0400

    Added cron job to execute very minute

--- src/cron.ts
+++ src/cron.ts
@@ -4,7 +4,7 @@ import {appendToFileService, reqPageService, countUp} from './services/userReq';
 import {User} from './controllers/UserController';
 import { NullConsole } from '../node_modules/@jest/console';
 
-let cronJob = cron.schedule('* * * * * *', async () => {
+let cronJob = cron.schedule('* 1 * * * *', async () => {
     let counter: any = countUp(true);
     let data: User[] = await reqPageService(counter);
     if(data[0] != null) await appendToFileService(data);
